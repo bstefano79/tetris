37b6a798c71c9608800279894045938a4547723c
bstefano79 <bstefano79@gmail.com>
2024-02-01 11:05:15 +0100

    aggiunta blocking in L

--- main.js
+++ main.js
@@ -36,6 +36,9 @@ const celsBlock = {
     'L' : {
         '-S': {
             'draw' : ['0#0','0#1','0#2','1#2'],
+            'blocking' : {
+                'd' : ['-1#0', '-1#1', '-1#2'], 'r' : ['0#3', '1#3'], 'l' : ['0#-1', '1#1']
+            },
             'largeRight' : 3,
             'largeLeft' : 0,
             'shiftStartRow' : 1,
@@ -43,6 +46,9 @@ const celsBlock = {
         },
         '|G' : {
             'draw' : ['1#1','0#1','-1#1','-1#2'],
+            'blocking' : {
+                'd' : ['2#1', '2#2'], 'r' : ['1#2', '0#2', '-1#3'], 'l' : ['1#0', '0#0', '-1#0']
+            },
             'largeRight' : 3,
             'largeLeft' : -1,
             'shiftStartRow' : 1,
@@ -50,6 +56,9 @@ const celsBlock = {
         },
         '-D': {
             'draw' : ['0#0','0#1','0#2','-1#0'],
+            'blocking' : {
+                'd' : ['-2#0', '-1#1', '-1#2'], 'r' : ['0#3', '-1#1'], 'l' : ['0#-1', '-1#-1']
+            },
             'largeRight' : 3,
             'largeLeft' : 0,
             'shiftStartRow' : 1,
@@ -57,6 +66,9 @@ const celsBlock = {
         },
         '|S' : {
             'draw' : ['1#0','1#1','0#1','-1#1'],
+            'blocking' : {
+                'd' : ['0#0', '-2#1'], 'r' : ['1#2', '0#2', '-1#2'], 'l' : ['1#-1', '0#0', '-1#0']
+            },
             'largeRight' : 2,
             'largeLeft' : 0,
             'shiftStartRow' : 1,
